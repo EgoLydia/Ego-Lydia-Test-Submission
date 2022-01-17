6d734f13f84aad3184345f4ed89916d4d197aad6
Lydia <egolydia97@gmail.com>
2022-01-18 00:40:34 +0100

    consumed API

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import SERVER, {ACCESS_KEY} from './server'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    images:[]
+  },
+  getters: {
+    images: state => {
+      return state.images
+    }
   },
   mutations: {
+    setImages(state, images) {
+      state.images = images
+    }
   },
   actions: {
+    fetchImages(context) {
+      const response = SERVER.get("photos?page=1", {
+        headers: {
+            Authorization: ACCESS_KEY
+        }
+      })
+      response.then((response)=>{
+        let images = []
+        response.data.forEach(image => {
+          images.push({
+            url: image.urls.full,
+            author: `${image.user.first_name} ${image.user.last_name}`,
+            location: image.user.location
+          })
+          
+        });
+        console.log(images)
+
+        context.commit('setImages', images)
+      }).catch((error)=>{
+        console.log(error)
+      })
+    }
   },
   modules: {
   }
---
+++ src/store/server.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+export const BASE_URL = process.env.VUE_APP_UNSPLASH_URL;
+export const ACCESS_KEY = process.env.VUE_APP_ACCESS_KEY;
+export const SERVER = axios.create({
+    baseURL: BASE_URL, 
+})
+
+export default SERVER
