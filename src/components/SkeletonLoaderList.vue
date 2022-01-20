b4d663fe7df7ba53357b52407221724fdee01769
Lydia <egolydia97@gmail.com>
2022-01-20 13:32:51 +0100

    Added skeleton loader list component

---
+++ src/components/SkeletonLoaderList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="loader-list">
+    <skeleton-loader
+      class="loader"
+      v-for="currentCount in count"
+      :key="currentCount"
+    ></skeleton-loader>
+  </div>
+</template>
+
+<script>
+import SkeletonLoader from "./SkeletonLoader.vue";
+
+export default {
+  name: "SkeletonLoaderList",
+  data() {
+    return {
+      count: 5,
+    };
+  },
+  components: {
+    "skeleton-loader": SkeletonLoader,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.loader-list {
+  column-gap: 3rem;
+  column-fill: initial;
+  column-count: 3;
+}
+
+.loader {
+  margin-bottom: 2rem;
+  vertical-align: top;
+  width: 100%;
+  height: 300px;
+}
+</style>
