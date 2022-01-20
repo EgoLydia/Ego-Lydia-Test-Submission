935f8aec0bf66cc8264fb38cf7e81318eb3355c1
Lydia <egolydia97@gmail.com>
2022-01-20 13:34:57 +0100

    Refactored staggered gallery

--- src/components/StaggeredGallery.vue
+++ src/components/StaggeredGallery.vue
@@ -1,14 +1,14 @@
 <template>
-      <div class="card-list">
-        <card
-          class="card-item"
-          v-for="image in images"
-          :key="image.url"
-          :image="image.url"
-          :author="image.author"
-          :location="image.location"
-        ></card>
-      </div>
+  <div class="card-list">
+    <card
+      class="card-item"
+      v-for="(image, index) in images"
+      :key="index"
+      :image="image.urls"
+      :author="image.author"
+      :location="image.location"
+    ></card>
+  </div>
 </template>
 
 <script>
@@ -16,19 +16,29 @@ import Card from "./Card.vue";
 
 export default {
   name: "StaggeredGallery",
-  props: ['images'],
+  props: ["images"],
   components: {
-    'card': Card,
+    card: Card,
   },
 };
 </script>
 
 <style scoped lang="scss">
 
-.card-list {
-  column-gap: 3rem;
-  column-fill: initial;
-  column-count: 3;
+@media only screen and (max-width: 933px) {
+  .card-list {
+    column-gap: 3rem;
+    column-fill: initial;
+    column-count: 1;
+  }
+}
+
+@media only screen and (min-width: 933px) {
+  .card-list {
+    column-gap: 3rem;
+    column-fill: initial;
+    column-count: 3;
+  }
 }
 
 .card-item {
