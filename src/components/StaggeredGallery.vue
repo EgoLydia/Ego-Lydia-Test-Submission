0f0cab5c6fa6e8455e0d8e32982ce67760e2302c
Lydia <egolydia97@gmail.com>
2022-01-20 08:32:27 +0100

    Created  a staggered grid layout

---
+++ src/components/StaggeredGallery.vue
@@ -0,0 +1,38 @@
+<template>
+      <div class="card-list">
+        <card
+          class="card-item"
+          v-for="image in images"
+          :key="image.url"
+          :image="image.url"
+          :author="image.author"
+          :location="image.location"
+        ></card>
+      </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+  name: "StaggeredGallery",
+  props: ['images'],
+  components: {
+    'card': Card,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.card-list {
+  column-gap: 3rem;
+  column-fill: initial;
+  column-count: 3;
+}
+
+.card-item {
+  margin-bottom: 2rem;
+  vertical-align: top;
+}
+</style>
