7f80acd98ce5ee1e7cb45386abc43332d632e76e
Lydia <egolydia97@gmail.com>
2022-01-20 08:31:57 +0100

    Created top bar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="top-bar-bg"> 
+        <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+  components: {
+  },
+  data(){
+    return {
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  .top-bar-bg {
+    background-color: #dde2e9;
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+  }
+  
+
+</style>
