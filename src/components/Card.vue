e58cae9d2ca13b7c790060f4de1b099c582ede62
Lydia <egolydia97@gmail.com>
2022-01-20 14:25:25 +0100

    Added gradient section

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -75,7 +75,7 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8));
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 1));
   width: 100%;
   height: 100%;
   display: flex;
